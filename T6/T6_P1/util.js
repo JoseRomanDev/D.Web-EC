80e849f241ff935c0fb7a36ddfb836946f0c0b59
JoseRomanDev <josemanuel.rnav@gmail.com>
2025-11-17 10:20:06 +0100

    Promises unfinished

---
+++ T6/T6_P1/util.js
@@ -0,0 +1,11 @@
+import Person from "./classes/Person";
+import { people } from "./mockdata";
+
+const objectParseToStr = (people) =>{
+    return new Promise((resolve,reject) =>{
+        
+    });   
+}
+
+objectParseToStr(people)
+.then(result)
